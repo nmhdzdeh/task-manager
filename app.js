b6ee255f9266aa09278a55b2fa2635fe80265936
nahal <mrym.mhdzde@gmail.com>
2024-12-04 14:30:49 +0330

    task routes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const tasksRoutes = require("./routes/tasks");
 
+app.use(express.json());
+
+app.use("/api/v1/tasks", tasksRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
