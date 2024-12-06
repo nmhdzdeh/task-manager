fcfae2b0d7060e9ba720342923c79d3e9f368d2f
nahal <mrym.mhdzde@gmail.com>
2024-12-06 11:25:27 +0330

    connected to mongoDB

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 const tasksRoutes = require("./routes/tasks");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasksRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+    await connectDB(process.env.MONGO_URL);
+    console.log("CONNECTED TO DB...");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
